34abf3fc8c562c8ae0f0c4f0e800858d9b977ed4
saadetsimsek <saadetsimsek13@gmail.com>
2025-05-25 23:33:49 +0300

    Initial Commit

---
+++ MyCallKitTests/MyCallKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCallKitTests.swift
+//  MyCallKitTests
+//
+//  Created by Saadet Şimşek on 25/05/2025.
+//
+
+import Testing
+@testable import MyCallKit
+
+struct MyCallKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
