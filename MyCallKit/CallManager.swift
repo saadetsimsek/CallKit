d784f01cc8cd1af07fb9c90bcaec25795329a067
saadetsimsek <saadetsimsek13@gmail.com>
2025-05-28 21:27:50 +0300

    reportIncomingCall

--- MyCallKit/CallManager.swift
+++ MyCallKit/CallManager.swift
@@ -8,7 +8,34 @@
 import Foundation
 import CallKit
 
-final class CallManager: NSObject {
+final class CallManager: NSObject, CXProviderDelegate {
+    
     let provider = CXProvider(configuration: CXProviderConfiguration())
     let cellController = CXCallController()
+    
+    override init() {
+        super.init()
+        provider.setDelegate(self, queue: nil)
+    }
+    
+    public func reportIncomingCall(id: UUID, handle: String){
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .generic, value: handle)
+        provider.reportNewIncomingCall(with: id, update: update) { error in
+            if let error = error {
+                print(String(describing: error))
+            }
+            else {
+                print("call reported")
+            }
+        }
+    }
+    
+    public func starCall(id: UUID, handle: String){
+        
+    }
+    
+    func providerDidReset(_ provider: CXProvider) {
+        
+    }
 }
